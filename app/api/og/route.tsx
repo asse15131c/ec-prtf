c2e8cbffaad0fcdd076b029c80491b756f3573f5
asse15131c <elena@sixsocks.studio>
2024-07-04 21:45:56 +0200

    fix: fixed og

--- app/api/og/route.tsx
+++ app/api/og/route.tsx
@@ -39,7 +39,6 @@ export async function GET() {
       }
     );
   } catch (e) {
-    console.log(`${e.message}`);
     return new Response(`Failed to generate the image`, {
       status: 500,
     });
