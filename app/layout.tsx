2ab9c85d391817702b1bbccdfbdb9aff26ce42be
asse15131c <elena@sixsocks.studio>
2024-07-06 09:05:58 +0200

    fix: refacored meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import localFont from "next/font/local";
 import Head from "next/head";
 
 import "./globals.css";
+import { Favicon } from "./components/Favicon";
 
 const neueHaasUnica = localFont({
   src: [
@@ -23,6 +24,12 @@ export const metadata: Metadata = {
   description: "Frontend developer based in Milan",
   referrer: "origin-when-cross-origin",
   keywords: ["Elena Catani", "Frontend developer"],
+  twitter: {
+    title: "Elena Catani",
+    description: "Frontend developer",
+    images: "http://elenacatani.com/api/og",
+    card: "summary_large_image",
+  },
 };
 
 export default function RootLayout({
@@ -33,39 +40,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/favicon/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/favicon/site.webmanifest" />
-        <link
-          rel="mask-icon"
-          href="/favicon/safari-pinned-tab.svg"
-          color="#000000"
-        />
-        <meta name="msapplication-TileColor" content="#ffffff" />
-        <meta name="theme-color" content="#ffffff"></meta>
+        <Favicon />
         <meta name="og:title" content="Elena Catani" />
         <meta name="og:description" content="Frontend developer" />
         <meta property="og:image" content="http://elenacatani.com/api/og" />
         <meta name="og:url" content="https://elenacatani.com" />
-        <meta name="twitter:title" content="Frontend developer" />
-        <meta name="twitter:description" content="Frontend developer" />
-        <meta name="twitter:image" content="http://elenacatani.com/api/og" />
-        <meta name="twitter:card" content="summary_large_image" />
       </head>
 
       <body className={neueHaasUnica.variable}>
